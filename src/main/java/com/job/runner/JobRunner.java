c50bafefc0166119065d83b607f6dd7a761f8a89
Frank Giordano <giofr01@yahoo.com>
2023-04-06 23:20:50 -0400

    update javadoc

--- src/main/java/com/job/runner/JobRunner.java
+++ src/main/java/com/job/runner/JobRunner.java
@@ -14,7 +14,7 @@ import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
- * Following project provides a way to define a set of jobs to be submitted together in like a multi batch process on
+ * Following program provides a way to define a set of jobs to be submitted together in like a multi batch process on
  * your z/OS instance(s). It can be used to automate a set of jobs to be submitted on a scheduled bases using for
  * instance Jenkins. Each job is submitted in an asynchronous way. As such, these jobs are submitted via a thread pool.
  * The advantage of a thread pool speeds up execution and completion time.
